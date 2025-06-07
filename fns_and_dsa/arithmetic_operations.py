62a316f186aa3ff7667b17f8474a235264d7d209
Prichard-1 <genarodube8@gmail.com>
2025-06-08 01:08:53 +0200

    add arithmethic file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,5 @@
 ithmetic_operations.py
-def perform_operation(num1:float, num2:float, operation: str):
+def perform_operation(num1:float, num2:float, operation):
     """performs basic arithmetic operations based on operation parameter."""
     if operation=="add":
         return num1 + num2
