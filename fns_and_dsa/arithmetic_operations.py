5ed661f33fdd74bebb8632d3d66beb8c62be094a
Prichard-1 <genarodube8@gmail.com>
2025-06-08 00:36:32 +0200

    add arithmethic_operations file

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+ithmetic_operations.py
+def perform_operation(num1:float, num2:float, operation: str):
+    """performs basic arithmetic operations based on operation parameter."""
+    if operation=="add":
+        return num1 + num2
+    elif operation=="subtract":
+        return num1 - num2
+    elif operation=="multipy":
+        return num1 * num2
+    elif operation=="devide"
+         if num2==0:
+             return "error: Division by zero is not allowed."
+         return num1/num2
+    else:
+        return "Error: Invad operation. Choose from 'add', 'subtract', 'multiply', or 'devide'."
+    
