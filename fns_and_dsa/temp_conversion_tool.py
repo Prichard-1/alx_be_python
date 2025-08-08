f3a36092af61fe147dc960e5bc2b84f82f98e9bc
Prichard Dube <genarodube8@gmail.com>
2025-08-09 00:06:49 +0200

    Add temperature conversion tool using global scope for conversion factors

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,12 +1,12 @@
 # temp_conversion_tool.py
 
 # Global Conversion Factors
-FAHRENHEHEIT_TO_CELSIUS_FACTOR = 5 / 9
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 # Conversion Functions
 def convert_to_celsius(fahrenheit):
-    return (fahrenheit - 32) * FAHRENHEHEIT_TO_CELSIUS_FACTOR
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
